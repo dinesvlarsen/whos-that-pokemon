8db46a1467b88ff3b0a4a7dcfc6c6d71cd68409f
Dines <dinesvlarsen@gmail.com>
2022-03-09 08:16:10 +0100

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+	<whosthatpokemon />
+</template>
+
+<script>
+import Whosthatpokemon from '../components/Whosthatpokemon.vue';
+export default {
+	data() {},
+	components: {
+		Whosthatpokemon,
+	},
+};
+</script>

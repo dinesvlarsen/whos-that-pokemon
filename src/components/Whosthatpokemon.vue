ae014720e0107184a8a11e677de20153ab4ed3ae
Dines <dinesvlarsen@gmail.com>
2022-03-15 11:01:36 +0100

    Made everything responsive

--- src/components/Whosthatpokemon.vue
+++ src/components/Whosthatpokemon.vue
@@ -1,6 +1,7 @@
 <template>
 	<div class="guess-pokemon">
 		<div class="guess-pokemon__image">
+			<!-- <img src="./../assets/images/25.png" alt="" /> -->
 			<img
 				:class="{ 'show-image': correctButtonStatus }"
 				:src="correctPokemon.image.url"
@@ -8,21 +9,30 @@
 			/>
 		</div>
 
-		<h1 class="pokemon-font">{{ pokemonOutput }}</h1>
+		<h1 class="guess-pokemon__output">{{ pokemonOutput }}</h1>
+
+		<!-- Was struggling with the button logic here, ended up going for an approach found here https://stackoverflow.com/questions/43956662/event-handling-dynamic-created-buttons-in-vue-js-v-for-loop where u add a boolean property to the objects used for the buttons. -->
+		<div class="guess-pokemon__buttons">
+			<button
+				@click="buttonOnClick(buttonIndex)"
+				class="guess-pokemon__button"
+				:class="{
+					'guess-pokemon__button--incorrect': gamePokemons[buttonIndex].clicked,
+					//prettier-ignore
+					'guess-pokemon__button--correct':gamePokemons[buttonIndex].correctClicked,
+				}"
+				v-for="(pokemon, buttonIndex) in this.gamePokemons"
+				:key="pokemon.buttonIndex"
+			>
+				<div>{{ pokemon.name }}</div>
+			</button>
+		</div>
 
 		<button
-			@click="buttonOnClick(buttonIndex)"
-			class="guess-pokemon__buttons"
-			:class="{
-				'guess-pokemon__button--incorrect': gamePokemons[buttonIndex].clicked,
-				//prettier-ignore
-				'guess-pokemon__button--correct':gamePokemons[buttonIndex].correctClicked,
-			}"
-			v-for="(pokemon, buttonIndex) in this.gamePokemons"
-			ref="buttons"
-			:key="pokemon.buttonIndex"
+			class="guess-pokemon__decorative-sprite guess-pokemon__decorative-sprite--hidden"
+			@click="spriteOnClick()"
 		>
-			<div>{{ pokemon.name }}</div>
+			<img :src="decorativeSprite.url" :alt="decorativeSprite.altText" />
 		</button>
 	</div>
 </template>
@@ -31,7 +41,7 @@
 export default {
 	data() {
 		return {
-			correctButtonStatus: false, 
+			correctButtonStatus: false,
 			correctPokemon: {
 				name: '',
 				correctAnswerIndex: null,
@@ -44,6 +54,10 @@ export default {
 			gamePokemons: [],
 			randomIndexesUsed: [], //Keeps track of what pokemons has been pushed into gamePokemons.
 			allPokemons: [],
+			decorativeSprite: {
+				url: '',
+				altText: '',
+			},
 		};
 	},
 
@@ -54,6 +68,7 @@ export default {
 
 		this.fetchPokemon().finally(() => {
 			this.runApp();
+			this.setDecorativeSprite();
 		});
 	},
 
@@ -70,6 +85,11 @@ export default {
 			}
 		},
 
+		spriteOnClick() {
+			this.setDecorativeSprite();
+		},
+
+		//Used when the correct button is clicked.
 		correctButtonClicked(buttonIndex) {
 			this.correctButtonStatus = true;
 			this.gamePokemons[buttonIndex].correctClicked = true;
@@ -83,7 +103,9 @@ export default {
 
 		//Method used to start the app.
 		runApp() {
+			//Checks if the game has gone through its first game, and if it has resets all the data cached.
 			if (this.gamePokemons.length > 0) this.resetApp();
+
 			this.getFourRandomPokemons();
 
 			// This sets all the data we need for the correctPokemon.
@@ -92,6 +114,8 @@ export default {
 
 		//Resets the data used in the app.
 		resetApp() {
+			//Deletes the clicked and correctClicked properties from the pokemons retrieved, -
+			// - since these properties also gets applied to the objects in allPokemons.
 			this.gamePokemons.forEach((pokemon) => {
 				delete pokemon.clicked;
 				delete pokemon.correctClicked;
@@ -104,11 +128,27 @@ export default {
 
 		//Fetches all the pokemons from the pokeApi and caches them.
 		async fetchPokemon() {
-			const url = 'https://pokeapi.co/api/v2/pokemon?limit=151';
-			const response = await fetch(url);
-			const { results } = await response.json();
-			this.allPokemons = results;
-			this.mutateAllPokemons();
+			try {
+				const url = 'https://pokeapi.co/api/v2/pokemon?limit=151';
+				const response = await fetch(url);
+				const { results } = await response.json();
+
+				this.allPokemons = results;
+				this.mutateAllPokemons();
+			} catch (error) {
+				//Solution for a dynamic img src with vite taken from https://stackoverflow.com/questions/66419471/vue-3-vite-dynamic-img-src
+				this.correctPokemon.image.url = new URL(
+					'./../assets/images/error.png',
+					import.meta.url
+				).href;
+				this.pokemonOutput = `Something went wrong! ${error}`;
+
+				console.error(
+					'⚠️⚠️⚠️⚠️⚠️⚠️ERROR ERROR ERROR DUDE LOOK HERE ERROR!⚠️⚠️⚠️⚠️⚠️' +
+						error
+				);
+				console.error(error.message);
+			}
 		},
 
 		//Adds an index to each pokemon in allPokemons, this index is used to get images from assets/images/poke-sprites-kanto folder.
@@ -173,6 +213,21 @@ export default {
 			}
 		},
 
+		//Creates a random pokemon image link to use as a decorativeSprite.url.
+		setDecorativeSprite() {
+			const randomIndex = this.randomIndex();
+
+			//Solution for a dynamic img src with vite taken from https://stackoverflow.com/questions/66419471/vue-3-vite-dynamic-img-src
+			const decorativeSprite = new URL(
+				`./../assets/images/poke-sprites-kanto/${randomIndex + 1}.png`,
+				import.meta.url
+			).href;
+			const decorativeSpriteAltText = `A decorative picture of ${this.allPokemons[randomIndex].name}`;
+
+			this.decorativeSprite.url = decorativeSprite;
+			this.decorativeSprite.altText = decorativeSpriteAltText;
+		},
+
 		//Generates a random number used for pokemonId, the number is used in the pokeapi http link to get a random pokemon.
 		randomIndex() {
 			let randomId = Math.floor(Math.random() * 150);
@@ -193,28 +248,17 @@ export default {
 </script>
 
 <style>
-.pokemon-font {
-	color: #ffcb05;
-	-webkit-text-stroke-width: 0.06em;
-	-webkit-text-stroke-color: #385aaa;
-	letter-spacing: 4px;
-	font-size: 20px;
-	text-align: center;
-	font-family: 'pokemon-font';
-	margin-bottom: 1rem;
-}
-
 .guess-pokemon {
-	margin: 1rem;
+	max-width: 1440px;
+	margin: 34px 24px 0 24px;
 }
 
 .guess-pokemon__image {
 	background-color: #62ddd5;
 	border-radius: 50%;
-	border: 4px solid #ffcb05;
-	width: 200px;
-	margin: 30px auto;
-	height: auto;
+	border: 8px solid var(--primary);
+	margin: 0 auto;
+	max-width: 199px;
 }
 
 .guess-pokemon__image img {
@@ -235,26 +279,95 @@ export default {
 	animation: showImage 2s forwards;
 }
 
-.guess-pokemon__buttons {
+.guess-pokemon__output {
+	color: #ffcb05;
+	-webkit-text-stroke-width: 1px;
+	-webkit-text-stroke-color: var(--secondary);
+	font-size: var(--output-font-size);
+	letter-spacing: 0.163em;
+	font-family: var(--output-font-family);
+	text-align: center;
+	margin-top: var(--m);
+}
+
+.guess-pokemon__button {
 	background-color: #ffcb05;
 	display: block;
 	color: rgb(54, 54, 54);
 	width: 100%;
-	max-width: 450px;
-	padding: 0.8em;
-	margin: 0.6rem auto;
+	max-width: 342px;
+	padding: 14px;
+	margin: var(--s) auto;
 	border-radius: 4px;
 	border: solid 2px rgba(0, 10, 66, 0.61);
-	font-family: 'Press Start 2P', cursive;
-	font-size: 14px;
+	font-size: var(--button-font-size);
 }
 
 .guess-pokemon__button--correct {
-	background-color: rgb(0, 180, 0);
+	background-color: var(--correct);
 }
 
 .guess-pokemon__button--incorrect {
-	background-color: rgb(207, 41, 41);
+	background-color: var(--incorrect);
 	text-decoration: line-through;
+}
+
+.guess-pokemon__decorative-sprite--hidden {
+	display: none;
+}
+
+.guess-pokemon__decorative-sprite {
+	position: relative;
+	bottom: 0;
+	left: 0;
+	width: 200px;
+}
+
+/* MEDIA QUERIES MOBILE FIRST */
+
+@media screen and (min-width: 744px) {
+	.guess-pokemon {
+		margin: 116px 24px 0 24px;
+	}
+
+	.guess-pokemon__image {
+		max-width: 303px;
+		margin-top: var(--l);
+	}
+
+	.guess-pokemon__output {
+		-webkit-text-stroke-width: 2px;
+		margin-top: var(--xl);
+		margin-bottom: var(--l);
+	}
+
+	.guess-pokemon__button {
+		margin-top: 0;
+		border: solid 3px var(--secondary);
+		max-width: 398px;
+	}
+
+	.guess-pokemon__decorative-sprite--hidden {
+		display: block;
+	}
+}
+
+@media screen and (min-width: 1440px) {
+	.guess-pokemon {
+		margin-left: auto;
+		margin-right: auto;
+	}
+
+	.guess-pokemon__buttons {
+		display: grid;
+		grid-template-columns: minmax(398px, auto) minmax(398px, auto);
+		grid-gap: 16px 64px;
+		margin: 0 auto;
+		justify-content: center;
+	}
+
+	.guess-pokemon__decorative-sprite {
+		position: absolute;
+	}
 }
 </style>
